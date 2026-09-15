65860ac23642d017017e63d5050033a851c54975
Eyub197 <eyubmehmed122@gmail.com>
2026-09-15 14:42:31 +0300

    human made code

--- index.ts
+++ index.ts
@@ -2,3 +2,19 @@ async function setNotification(amountOfRetainers: number): Promise<void> {
 	const message: string = `Put on your retainer${amountOfRetainers < 1 ? "s" : ""}`;
 	await Bun.$`notify-send -t 5000 ${amountOfRetainers} '${message}'`;
 }
+
+function isRetainerTime(): boolean {
+	const date = new Date();
+	const hours = date.getHours();
+	const minutes = date.getMinutes();
+
+	if (hours === 14 && minutes === 21) {
+		return true;
+	}
+
+	return false;
+}
+
+// TODO read the json value from the file
+// every time switch it back
+// ! edge case : what if the app is opened 2 times a day
