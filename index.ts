6dd12bb9f1f3a85e7153c028f4a45bf1c23af71e
Eyub197 <eyubmehmed122@gmail.com>
2026-09-17 14:52:59 +0300

    ednge case handled bare bones almose done

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+const today = new Date().getDate();
+
 async function setNotification(amountOfRetainers: number): Promise<void> {
 	const message: string = `Put on your retainer${amountOfRetainers < 1 ? "s" : ""}`;
 	await Bun.$`notify-send -t 5000 ${amountOfRetainers} '${message}'`;
@@ -19,20 +21,22 @@ async function getValueFromStorege() {
 	const path = "storage.json";
 	const file = Bun.file(path);
 
-	const { amount } = await file.json();
-	return amount;
+	const { amount, date } = await file.json();
+	return { amount, date };
 }
 
 async function changeValueInStorege() {
-	const currentValue = await getValueFromStorege();
-	const newValue = currentValue === 1 ? 2 : 1;
+	const { amount, date } = await getValueFromStorege();
 
+	if (date === today) {
+		return;
+	}
+
+	const newValue = amount === 1 ? 2 : 1;
 	await Bun.write(
 		"storage.json",
-		JSON.stringify({ amount: newValue }, null, 2),
+		JSON.stringify({ amount: newValue, date: today }, null, 2),
 	);
 }
 
 changeValueInStorege();
-
-// ! edge case : what if the app is opened 2 times a day
